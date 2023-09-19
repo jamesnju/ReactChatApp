60483735f85a907c82a92e6308b31531b4f93cb7
jamesnju <james500nju@gmail.com>
2023-09-19 15:52:50 +0300

    first commit

--- src/components/MyMessage.jsx
+++ src/components/MyMessage.jsx
@@ -4,11 +4,17 @@ export const MyMessage = ({message}) => {
     if(message?.attachments?.length > 0){
         return (
             <img 
-                src={message.attachments[0]}
+                src={message.attachments[0].file}
+                alt='message-attachment'
+                className='message-image'
+                style={{float: 'right'}}
             />
         )
     }
   return (
-    <div>MyMessage</div>
+    <div className='message' 
+        style={{float:'right', marginRight: '18px', color: "gainsboro" , backgroundColor: '#3B2A50'}}>
+        {message.text}
+    </div>
   )
 }
